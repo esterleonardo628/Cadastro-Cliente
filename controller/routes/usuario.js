748308a649d13a27a1e5a85fa5382a162f42c38d
Ester Leonardo <ester.leonardo@fatec.sp.gov.br>
2022-05-20 22:23:05 -0300

    Att

--- controller/routes/usuario.js
+++ controller/routes/usuario.js
@@ -33,3 +33,58 @@ module.exports = function (app) {
         }
     })
 }
+
+
+app.post('/cadastro/usuario/edit/salvar', (req, res) => {
+    var usuario = {nome: req.body.nome, 
+    senha: req.body.senha,
+    id: req.body.id};
+
+    try {
+        usuarioBanco.updateUsuario(usuario);
+        res.render('usuario/Sucesso', { mensagem: 'alterado'});
+    }catch (error) {
+        res.render('usuario/EditUsuario', { title: 'Edição Cadastro',
+    mensagem: 'Erro no cadastrado'});
+    }
+
+});
+
+app.post('/cadastro/usuario/salvar', (req,res) => {
+    try {
+        var usuario = {nome: req.body.nome,
+            senha: seguranca.ocultarSenha(req.body.senha)}
+        usuarioBanco.insertUsuario(usuario);
+        res.render('usuario/Cadastro', {title: 'Cadastro,
+        mensagem: 'Erro no Cadastro'});
+    }
+});
+
+app.get('lista/Usuario', async (req, res, next) => {
+    try {
+        const docs = await usuarioBanco.selectUsuario();
+        res.render('usuario/Lista', {mensagem: 'Lista de Usuários', docs});
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.get('lista/Usuario', async (req, res, next) => {
+    try {
+        const docs = await usuarioBanco.selectUsuario();
+        res.render('usuario/Lista', {mensagem: 'Lista de Usuários', docs});
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.get('delete/usuario/:id', async (req, res, next) => {
+    try {
+        var id = req.params.id;
+        await usuarioBanco.deleteUsuario(id);
+        const docs = await usuarioBanco.selectUsuario();
+        res.render('usuario/Lista', { mensagem: 'usuario excluiodo com sucesso', docs });
+    } catch (err) {
+        next(err);
+    }
+});
--- model/repositories/usuarioBD.js
+++ model/repositories/usuarioBD.js
@@ -50,37 +50,3 @@ module.exports = {
     inserUsuario,
     deleteUsuario
 }
-
-app.post('/cadastro/usuario/edit/salvar', (req, res) => {
-    var usuario = {nome: req.body.nome, 
-    senha: req.body.senha,
-    id: req.body.id};
-
-    try {
-        usuarioBanco.updateUsuario(usuario);
-        res.render('usuario/Sucesso', { mensagem: 'alterado'});
-    }catch (error) {
-        res.render('usuario/EditUsuario', { title: 'Edição Cadastro',
-    mensagem: 'Erro no cadastrado'});
-    }
-
-});
-
-app.post('/cadastro/usuario/salvar', (req,res) => {
-    try {
-        var usuario = {nome: req.body.nome,
-            senha: seguranca.ocultarSenha(req.body.senha)}
-        usuarioBanco.insertUsuario(usuario);
-        res.render('usuario/Cadastro', {title: 'Cadastro,
-        mensagem: 'Erro no Cadastro'});
-    }
-});
-
-app.get('lista/Usuario', async (req, res, next) => {
-    try {
-        const docs = await usuarioBanco.selectUsuario();
-        res.render('usuario/Lista', {mensagem: 'Lista de Usuários', docs});
-    } catch (err) {
-        next(err);
-    }
-});
